861319831ad591d99ac20a84fb58ef7cd2b42336
Aleksandr_Sh. <a.shegai91@gmail.com>
2025-09-18 19:51:15 +0300

    pdf-compose

---
+++ go.mod
@@ -0,0 +1,32 @@
+module github.com/shegai01/pdf-compose
+
+go 1.22
+
+require (
+	github.com/go-openapi/errors v0.19.7
+	github.com/go-openapi/loads v0.19.5
+	github.com/go-openapi/runtime v0.19.22
+	github.com/go-openapi/spec v0.19.9
+	github.com/go-openapi/strfmt v0.19.5
+	github.com/go-openapi/swag v0.19.9
+	github.com/jessevdk/go-flags v1.4.0
+	github.com/jung-kurt/gofpdf v1.16.2
+	golang.org/x/net v0.0.0-20200925080053-05aa5d4ee321
+)
+
+require (
+	github.com/PuerkitoBio/purell v1.1.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20200428143746-21a406dcc535 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/go-openapi/analysis v0.19.10 // indirect
+	github.com/go-openapi/jsonpointer v0.19.3 // indirect
+	github.com/go-openapi/jsonreference v0.19.4 // indirect
+	github.com/go-openapi/validate v0.19.11 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/mailru/easyjson v0.7.1 // indirect
+	github.com/mitchellh/mapstructure v1.3.2 // indirect
+	go.mongodb.org/mongo-driver v1.3.4 // indirect
+	golang.org/x/text v0.3.3 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
---
+++ pkg/composer/pdf_compose.go
@@ -0,0 +1,90 @@
+package composer
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/jung-kurt/gofpdf"
+)
+
+func ComposeFromFiles(files []io.ReadCloser) (io.ReadCloser, error) {
+	var opt gofpdf.ImageOptions
+	pdf := gofpdf.New("P", "mm", "A4", "")
+
+	dir, err := os.MkdirTemp("", "pdfcomp")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	for i, f := range files {
+		if f == nil {
+			continue
+		}
+
+		tmpfn := filepath.Join(dir, fmt.Sprintf("tmpfile_%d", i))
+		data, _ := io.ReadAll(f)
+		if err := os.WriteFile(tmpfn, data, 0666); err != nil {
+			log.Fatal(err)
+		}
+
+		tmpf, _ := os.Open(tmpfn)
+
+		if tmpf == nil {
+			continue
+		}
+
+		m, format, err := image.DecodeConfig(tmpf)
+		if err != nil {
+			continue
+		}
+
+		tmpf.Seek(0, 0)
+		weigh, height := calculateRotation(m.Width, m.Height)
+		if weigh > 0 {
+			pdf.AddPage()
+		}
+
+		_ = pdf.RegisterImageReader("file"+strconv.Itoa(i), format, tmpf)
+		pdf.ImageOptions("file"+strconv.Itoa(i), 0, 0, weigh, height, true, opt, 0, "")
+		tmpf.Close()
+	}
+
+	fb := []byte{}
+	rw := bytes.NewBuffer(fb)
+
+	err = pdf.Output(rw)
+	if err != nil {
+		return nil, err
+
+	}
+
+	o := io.NopCloser(rw)
+	return o, nil
+
+}
+
+func calculateRotation(w, h int) (width, height float64) {
+	x := float32(w)
+	y := float32(h)
+	if x >= y {
+		return 210, 0
+	}
+
+	f := x / 100
+	percent := y / f
+	diff := percent - 100
+	if diff > 41 {
+		return 0, 280
+
+	}
+
+	return 210, 0
+
+}
